178e14b65dec0b0b0604c0072044a16ad6a043c8
Holiday Course <holiday@ad.aie.edu>
2018-08-13 16:28:59 -0500

    Add project files.

---
+++ IntroToCpp/main.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+
+int main()
+{
+
+	 std::cout << "Joshua Hagan" << std::endl;
+	 std::cout << "Strawberry" << std::endl;
+	 std::cout << "Kiwi" << std::endl;
+	 std::cout << "Apple" << std::endl;
+
+
+	 std::cout << std::endl;
+
+
+	 std::cout << "1.California\n";
+	 std::cout << "2.Lucid Dreams\n";
+	 std::cout << "3.Candles\n";
+	 std::cout << "4.All Girls Are The Same\n";
+	 std::cout << "5.Naruto Opening 1\n";
+	 std::cout << "6.Yes Indeed\n";
+	 std::cout << "7.Soutside\n";
+	 std::cout << "8.My Dawg\n";
+	 std::cout << "9.Let You Down\n";
+	 std::cout << "10.SAD!\n";
+
+
+	 std::cout << std::endl;
+
+	 std::cout << "0\n";
+	 std::cout << "1\n";
+	 std::cout << "2\n";
+	 std::cout << "3\n";
+	 std::cout << "4\n";
+	 std::cout << "5\n";
+	 
+	 std::cout << std::endl;
+
+	 std::cout << "5\n";
+	 std::cout << "4\n";
+	 std::cout << "3\n";
+	 std::cout << "2\n";
+	 std::cout << "1\n";
+	 std::cout << "0\n";
+
+
+	system("pause");
+
+}
